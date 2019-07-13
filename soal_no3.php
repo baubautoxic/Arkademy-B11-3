daa75c3697a18594dfb60ef82a499aeb86a065c5
baubautoxic <52845149+baubautoxic@users.noreply.github.com>
2019-07-13 22:24:51 +0700

    Update soal_no3.php

--- soal_no3.php
+++ soal_no3.php
@@ -1,11 +1,34 @@
 <?php
-    function kataxfrasa($kata, $frasa)
-{
-    $jmlStrkata = strlen($kata);
-    $jmlStrfrasa = strlen($frasa);
-	$jlmkatafrasa = (substr_count($kata, $frasa)*3);
-
-        echo "Ditemukan ".$jlmkatafrasa." kali";
+function kataxfrasa($kata,$frasa){
+        $kata1 = $kata;
+        $kata2= strrev($kata1);
+        $cari = $frasa;
+        $jumlah_kata = 0;
+        $arr_kata1 = str_split($kata1);
+        $arr_kata2 = str_split($kata2);
+        $arr_cari = str_split($cari);
+        $jml_kata = count($arr_kata2);
+        $jml_cari = count($arr_cari);
+        for ($i = 0; $i <= ($jml_kata - $jml_cari); $i++) {
+            $frasa = "";
+            for ($j = $i; $j < ($i + $jml_cari); $j++) {
+                $frasa .= $arr_kata1[$j];
+            }
+            if ($frasa == $cari) {
+                $jumlah_kata++;
+            }
+        }
+        
+        for ($i = 0; $i <= ($jml_kata - $jml_cari); $i++) {
+            $frasa = "";
+            for ($j = $i; $j < ($i + $jml_cari); $j++) {
+                $frasa .= $arr_kata2[$j];
+            }
+            if ($frasa == $cari) {
+                $jumlah_kata++;
+            }
+        }
+        echo "ditemukan " . $jumlah_kata . " kali";
 }
-kataxfrasa("banananana","nana");
+kataxfrasa('banananana','nana');
 ?>
